74bbe983c0fb177fb2fced1aff3bf74887ce5341
Flávio Couto <flavio.couto@live.com>
2025-06-02 18:47:57 -0300

    TASK - Início

---
+++ C4PRESENTATION_WEB/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace C4PRESENTATION_WEB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
